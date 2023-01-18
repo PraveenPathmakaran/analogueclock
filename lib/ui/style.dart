b08edfeb96871f4fa6097835aa9110f81ea0ca29
Praveen Pathmakaran <praveenpathmakaran@gmail.com>
2023-01-18 22:06:45 +0530

    clock ui complete

---
+++ lib/ui/style.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/services.dart';
+
+class AppStryle {
+  static Color primaryColor = const Color(0xFF2253FF);
+  static Color primaryColorDark = const Color(0xFF2253FF);
+}
---
+++ lib/utils/time_model.dart
@@ -0,0 +1,7 @@
+class TimeModel {
+  int? sec;
+  int? min;
+  int? hour;
+
+  TimeModel(this.sec, this.min, this.hour);
+}
